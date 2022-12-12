7358879210e3a3877ec3cedbdc5eec10c560970a
ScepterDev01 <Scepterboss@gmail.com>
2022-12-12 10:56:43 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,4 @@
 #include <stdio.h>
-
-
-
 /**
  *
  *  * main - Entry point
@@ -15,15 +12,10 @@
  *      * Return: Always 0 (Success)
  *
  *      */
-
-
-
 int main(void)
 
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
