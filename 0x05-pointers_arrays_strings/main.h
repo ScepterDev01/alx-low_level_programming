71a3b25d1a36ed43a2de787a2c25ea507c33b03b
ScepterDev01 <Scepterboss@gmail.com>
2022-12-19 17:01:32 -0800

    great

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,32 +1,65 @@
-#ifndef MAIN_H
+#ifndef MAIN_H                                                                                                              
 
-#define MAIN_H
+                                                                                                                            
 
-#include <stdio.h>
+#define MAIN_H                                                                                                              
 
-void reset_to_98(int *n);
+                                                                                                                            
 
-int _putchar(char c);
+#include <stdio.h>                                                                                                          
 
-void swap_int(int *a, int *b);
+                                                                                                                            
 
-int _strlen(char *s);
+void reset_to_98(int *n);                                                                                                   
 
-void _puts(char *str);
+                                                                                                                            
 
-void print_rev(char *s);
+int _putchar(char c);                                                                                                       
 
-void rev_string(char *s);
+                                                                                                                            
 
-void puts2(char *str);
+void swap_int(int *a, int *b);                                                                                              
 
-void puts_half(char *str);
+                                                                                                                            
 
-void print_array(int *a, int n);
+int _strlen(char *s);                                                                                                       
 
-char *_strcpy(char *dest, char *src);
+                                                                                                                            
 
-int _atoi(char *s);
+void _puts(char *str);                                                                                                      
 
+                                                                                                                            
 
-#endif /* MAIN_H */
+void print_rev(char *s);                                                                                                    
+
+                                                                                                                            
+
+void rev_string(char *s);                                                                                                   
+
+                                                                                                                            
+
+void puts2(char *str);                                                                                                      
+
+                                                                                                                            
+
+void puts_half(char *str);                                                                                                  
+
+                                                                                                                            
+
+void print_array(int *a, int n);                                                                                            
+
+                                                                                                                            
+
+char *_strcpy(char *dest, char *src);                                                                                       
+
+                                                                                                                            
+
+int _atoi(char *s);                                                                                                         
+
+                                                                                                                            
+
+                                                                                                                            
+
+#endif /* MAIN_H */                                                                                                         
+
+~                    
