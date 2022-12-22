4f8efd836b65d8c395ba2d0363f87e7eeb9bee92
ScepterDev01 <scepterboss@gmail.com>
2022-12-22 13:11:39 -0800

    strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,36 +1,38 @@
+MAIN.H file
+
+
+
 #ifndef MAIN_H
 
 #define MAIN_H
 
 
 
+int _putchar(char c);
 
+char *_strcat(char *dest, char *src);
 
-int _putchar(char c)
+char *_strncat(char *dest, char *src, int n);
 
-	char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
 
-	char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
-	char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
 
-	int _strcmp(char *s1, char *s2);
+char *string_toupper(char *a);
 
-	void reverse_array(int *a, int n);
+char *cap_string(char *a);
 
-	char *string_toupper(char *);
+char *leet(char *a);
 
-	char *cap_string(char *);
+char *rot13(char *a);
 
-	char *leet(char *);
+void print_number(int n);
 
-	char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-	void print_number(int n);
-
-	char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
-	void print_buffer(char *b, int size);
+void print_buffer(char *b, int size);
 
 
 
